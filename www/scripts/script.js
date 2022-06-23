2b8a6ada2e3165163f37a01a4a82df20042f597e
Kirill Demidov <kademidov@gmail.com>
2022-06-23 14:01:18 +0300

    Реализованы табы, фильтрация в работах, подключена и инииализирована slick карусель

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -14,9 +14,74 @@ $(document).ready(function(){
 
 
   // FAQ
+  let prevFaqBtn;
+
   $('.js-faq-btn').on('click', function(){
 
-    $(this).next().slideToggle();
+    if (this === prevFaqBtn) {
+      $(this).toggleClass('open');
+      $(this).next().slideToggle();
+    } else {
+      prevFaqBtn = this;
+      $('.js-faq-btn').next().slideUp();
+      $('.js-faq-btn').removeClass('open');
+      $(this).next().slideDown();
+      $(this).addClass('open');
+    }
   });
+
+
+
+  // Табы в контактах
+  const activeClass = 'active';
+  const tabLinksClass = '.js-tabs-link';
+  const $tabLinks = $(tabLinksClass);
+  const $tabsContent = $('.js-contacts-content');
+
+  $tabLinks.on('click', function(event){
+    event.preventDefault();
+
+    $tabLinks.removeClass(activeClass);
+    $(this).addClass(activeClass);
+
+    let index = $(this).index(tabLinksClass);
+
+    $tabsContent.removeClass(activeClass);
+    $tabsContent.eq(index).addClass(activeClass);
+  });
+
+
+  // Фильтр в работах
+  $('.js-filter-link').on('click', function(event){
+    event.preventDefault();
+    let dataFilter = $(this).data('filter');
+
+    $('.js-filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    if (dataFilter === 'all') {
+      $('.js-works-item').show();
+
+      return;
+    }
+
+    $('.js-works-item').each(function() {
+      let dataType = $(this).data('type');
+      console.log(dataType);
+
+      if (dataType === dataFilter) {
+        $(this).show();
+
+        return;
+      }
+
+      $(this).hide();
+    });
+  });
+
+
+  // карусель
+
+  $('.js-carousel').slick();
 
 });
